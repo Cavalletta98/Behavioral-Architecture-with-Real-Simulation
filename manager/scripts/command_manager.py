34bcf1783869f1be9b8f52b7a72f0e71fe0babbb
Simone <simone.voto98@gmail.com>
2020-11-28 15:10:02 +0100

    Implement robot position control via action server

--- manager/scripts/command_manager.py
+++ manager/scripts/command_manager.py
@@ -12,152 +12,14 @@ import smach
 import smach_ros
 import time
 import random
-from geometry_msgs.msg import Point
-from std_msgs.msg import String
-from robot_control.srv import TargetPos
+from geometry_msgs.msg import Point,PoseStamped
+import actionlib
+from robot_control.msg import RobotPlanningAction, RobotPlanningActionGoal
 
-## 2D home position
-home_pos = Point(rospy.get_param("home_pos_x"),rospy.get_param("home_pos_y"),0)
+min_transition_normal_sleep = 1
+max_transition_normal_sleep = 10
 
-## 2D person position
-person_pos = Point(rospy.get_param("person_pos_x"),rospy.get_param("person_pos_y"),0)
-
-## x coordinate of the map
-map_x = rospy.get_param("map_x")
-## y coordinate of the map
-map_y = rospy.get_param("map_y")
-
-## Min delay for transition between PLAY and NORMAL states
-min_transition_play_normal = rospy.get_param("min_transition_play_normal")
-
-## Max delay for transition between PLAY and NORMAL states
-max_transition_play_normal = rospy.get_param("max_transition_play_normal")
-
-## Min delay for transition between NORMAL and SLEEP states
-min_transition_normal_sleep = rospy.get_param("min_transition_normal_sleep")
-
-## Max delay for transition between NORMAL and SLEEP states
-max_transition_normal_sleep = rospy.get_param("max_transition_normal_sleep")
-
-## Min delay for SLEEP state
-min_sleep_delay = rospy.get_param("min_sleep_delay")
-
-## Max delay for SLEEP state
-max_sleep_delay = rospy.get_param("max_sleep_delay")
-
-# define state Play
-class play(smach.State):
-
-    """
-        A class used to represent the PLAY behaviour
-        of the robot
-
-        Attributes
-        --------
-        @param transition_value: define the value at wich it is performed the state transition
-        @type transition_value: int
-
-        @param transition: transition value to decide whenever we have to change state
-        @type transition: int
-
-        @param count: value to count how many times we perform the PLAY state before make a transition
-        @type count: int
-
-        Methods
-        -----
-        target_pos_client(x, y):
-            Makes a request to motion server using the target position (x,y) 
-            and wait for the response
-        getGesture(data)
-            Callback method that receives the pointed gesture and perform the PLAY 
-            behaviour
-        execute()
-            It publishes the person position and, after the robot reaches the position, it
-            subsribes to "gesture" topic. At the end, it waits for the state transition
-    """
-
-    def __init__(self):
-        # initialisation function, it should not wait
-
-        """
-            Constrcutor. It inizializes the attribute
-        """
-        smach.State.__init__(self,outcomes=['someTimes'])
-
-        ## Define the value at wich it is performed the state transition
-        self.transition_value = random.randint(min_transition_play_normal,max_transition_play_normal)
-        
-    def target_pos_client(self,x, y):
-
-        """
-            Makes a request to motion server using the target position (x,y) 
-            and wait for the response
-
-            @param x: x coordinate of the target position
-            @type x: int
-            @param y: y coordinate of the target position
-            @type y: int
-
-        """
-
-        rospy.wait_for_service('target_pos')
-        try:
-            target_pos = rospy.ServiceProxy('target_pos', TargetPos)
-            resp = target_pos(x, y)
-            return resp
-        except rospy.ServiceException as e:
-            rospy.logerr("Service call failed: %s",e)
-    
-    def getGesture(self,data):
-
-        """
-            Callback method that receives the pointed gesture and perform the PLAY 
-            behaviour
-
-            @param data: 2D pointed gesture
-            @type data: Point
-        """
-
-        if self.transition == 0:
-            self.target_pos_client(data.x,data.y)
-            rospy.loginfo("Robot arrived in (%d,%d)",data.x,data.y)
-            self.target_pos_client(person_pos.x,person_pos.y)
-            rospy.loginfo("Robot arrived in person position (%d,%d)",person_pos.x,person_pos.y)
-            self.count += 1
-            if self.count == self.transition_value:
-                self.transition = 1
-
-        
-    def execute(self, userdata):
-
-        """
-            It publishes the person position and, after the robot reaches the position, it
-            subsribes to "gesture" topic. At the end, it waits for the state transition
-
-            @param userdata: used to pass data between states
-            @type userdata: list
-        """
-
-        # function called when exiting from the node, it can be blacking
-
-        ## Transition value to decide whenever we have to change state
-        self.transition = 0
-
-        ## Value to count how many times we perform the PLAY state before make a transition
-        self.count = 0
-        self.transition_value = random.randint(min_transition_play_normal,max_transition_play_normal)
-        rospy.loginfo('Executing state PLAY')
-
-        self.target_pos_client(person_pos.x,person_pos.y)
-
-        rospy.loginfo("Robot arrived in person position (%d,%d)",person_pos.x,person_pos.y)
-
-        sub_gesture = rospy.Subscriber("gesture", Point, self.getGesture)
-
-        while self.transition == 0:
-            pass
-        sub_gesture.unregister()
-        return 'someTimes'
+home_pos = Point(5,5,0)
 
 # define state Sleep
 class sleep(smach.State):
@@ -200,14 +62,26 @@ class sleep(smach.State):
             @type y: int
 
         """
+        client = actionlib.SimpleActionClient('/robot_reaching_goal', RobotPlanningAction)
 
-        rospy.wait_for_service('target_pos')
-        try:
-            target_pos = rospy.ServiceProxy('target_pos', TargetPos)
-            resp = target_pos(x, y)
-            return resp
-        except rospy.ServiceException as e:
-            rospy.logerr("Service call failed: %s",e)
+        # Waits until the action server has started up and started
+        # listening for goals.
+        client.wait_for_server()
+
+
+        # Creates a goal to send to the action server.
+        goal = RobotPlanningActionGoal()
+        goal.goal.target_pose.pose.position.x = random.uniform(-10,10)
+        goal.goal.target_pose.pose.position.y = random.uniform(-10,10)
+
+        # Sends the goal to the action server.
+        client.send_goal(goal.goal)
+
+        # Waits for the server to finish performing the action.
+        client.wait_for_result()
+
+        # Prints out the result of executing the action
+        return client.get_result()
         
     def execute(self, userdata):
 
@@ -222,9 +96,10 @@ class sleep(smach.State):
         # function called when exiting from the node, it can be blacking
         rospy.loginfo('Executing state SLEEP')
 
-        self.target_pos_client(home_pos.x,home_pos.y)
+
+        result = self.target_pos_client(home_pos.x,home_pos.y)
+        rospy.loginfo("Robot arrived in home(%d,%d)",home_pos.x,home_pos.y)
         
-        rospy.loginfo("Robot arrived in home (%d,%d)",home_pos.x,home_pos.y)
         return 'wakeUp'
     
 
@@ -261,9 +136,8 @@ class normal(smach.State):
             Constrcutor. It subscribes to "command" topic
         """
 
-        smach.State.__init__(self,outcomes=['play','someTimes'])
-        rospy.Subscriber("command", String, self.getCommand)
-   
+        smach.State.__init__(self,outcomes=['someTimes'])
+
     def target_pos_client(self,x, y):
 
         """
@@ -276,26 +150,25 @@ class normal(smach.State):
             @type y: int
 
         """
+        client = actionlib.SimpleActionClient('/robot_reaching_goal', RobotPlanningAction)
 
-        rospy.wait_for_service('target_pos')
-        try:
-            target_pos = rospy.ServiceProxy('target_pos', TargetPos)
-            resp = target_pos(x, y)
-            return resp
-        except rospy.ServiceException as e:
-            rospy.logerr("Service call failed: %s",e)
+        # Waits until the action server has started up and started
+        # listening for goals.
+        client.wait_for_server()
 
-    def getCommand(self,data):
+        # Creates a goal to send to the action server.
+        goal = RobotPlanningActionGoal()
+        goal.goal.target_pose.pose.position.x = random.uniform(-10,10)
+        goal.goal.target_pose.pose.position.y = random.uniform(-10,10)
 
-        """
-            Callback method that received the "play" command and set the attribute
-            play to 1
+        # Sends the goal to the action server.
+        client.send_goal(goal.goal)
 
-            @param data: command message
-            @type data: str
-        """
+        # Waits for the server to finish performing the action.
+        client.wait_for_result()
 
-        self.play = 1
+        # Prints out the result of executing the action
+        return client.get_result()
 
     def execute(self, userdata):
 
@@ -306,24 +179,15 @@ class normal(smach.State):
             @param userdata: used to pass data between states
             @type userdata: list
         """
-
-        ## Variable that will be set to 1 when the command "play" arrives
-        self.play = 0
-
         rospy.loginfo('Executing state NORMAL')
 
         count_value = random.randint(min_transition_normal_sleep,max_transition_normal_sleep)
 
         for count in range(0,count_value):
-            position = Point()
-            position.x = random.randint(0,map_x)
-            position.y = random.randint(0,map_y)
-            self.target_pos_client(position.x,position.y)
-
-            rospy.loginfo("Robot arrived in (%d,%d)",position.x,position.y)
-            if self.play == 1:
-                rospy.loginfo("User says PLAY")
-                return 'play'
+            x = random.uniform(-10,10)
+            y = random.uniform(-10,10)
+            result = self.target_pos_client(x,y)
+            rospy.loginfo("Robot arrived in (%d,%d)",x,y)
 
         return 'someTimes'
 
@@ -345,12 +209,11 @@ def main():
     with sm:
         # Add states to the container
         smach.StateMachine.add('NORMAL', normal(), 
-                               transitions={'play':'PLAY', 
-                                            'someTimes':'SLEEP'})
+                               transitions={'someTimes':'SLEEP'})
         smach.StateMachine.add('SLEEP', sleep(), 
                                transitions={'wakeUp':'NORMAL'})
-        smach.StateMachine.add('PLAY', play(), 
-                               transitions={'someTimes':'NORMAL'})
+        #smach.StateMachine.add('PLAY', play(), 
+                               #transitions={'someTimes':'NORMAL'})
         
     # Create and start the introspection server for visualization
     sis = smach_ros.IntrospectionServer('server_name', sm, '/SM_ROOT')
---
+++ robot_control/scripts/go_to_point_robot.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python3
+
+# import ros stuff
+import rospy
+from geometry_msgs.msg import Twist, Point, Pose
+from nav_msgs.msg import Odometry
+from tf import transformations
+import math
+import actionlib
+import actionlib.msg
+import robot_control.msg
+
+# robot state variables
+position_ = Point()
+pose_ = Pose()
+yaw_ = 0
+# machine state
+state_ = 0
+# goal
+desired_position_ = Point()
+desired_position_.z = 0
+# parameters
+yaw_precision_ = math.pi / 9  # +/- 20 degree allowed
+yaw_precision_2_ = math.pi / 90  # +/- 2 degree allowed
+dist_precision_ = 0.1
+kp_a = -3.0  ## In ROS Noetic, it may be necessary to change the sign of this proportional controller
+kp_d = 0.2
+ub_a = 0.6
+lb_a = -0.5
+ub_d = 0.6
+
+# publisher
+pub = None
+
+# action_server
+act_s = None
+
+# callbacks
+
+
+def clbk_odom(msg):
+    global position_
+    global pose_
+    global yaw_
+
+    # position
+    position_ = msg.pose.pose.position
+    pose_ = msg.pose.pose
+
+    # yaw
+    quaternion = (
+        msg.pose.pose.orientation.x,
+        msg.pose.pose.orientation.y,
+        msg.pose.pose.orientation.z,
+        msg.pose.pose.orientation.w)
+    euler = transformations.euler_from_quaternion(quaternion)
+    yaw_ = euler[2]
+
+
+def change_state(state):
+    global state_
+    state_ = state
+    print ('State changed to [%s]' % state_)
+
+
+def normalize_angle(angle):
+    if(math.fabs(angle) > math.pi):
+        angle = angle - (2 * math.pi * angle) / (math.fabs(angle))
+    return angle
+
+
+def fix_yaw(des_pos):
+    global yaw_, pub, yaw_precision_2_, state_
+    desired_yaw = math.atan2(des_pos.y - position_.y, des_pos.x - position_.x)
+    err_yaw = normalize_angle(desired_yaw - yaw_)
+    rospy.loginfo(err_yaw)
+
+    twist_msg = Twist()
+    if math.fabs(err_yaw) > yaw_precision_2_:
+        twist_msg.angular.z = kp_a*err_yaw
+        if twist_msg.angular.z > ub_a:
+            twist_msg.angular.z = ub_a
+        elif twist_msg.angular.z < lb_a:
+            twist_msg.angular.z = lb_a
+
+    pub.publish(twist_msg)
+
+    # state change conditions
+    if math.fabs(err_yaw) <= yaw_precision_2_:
+        print ('Yaw error: [%s]' % err_yaw)
+        change_state(1)
+
+
+def go_straight_ahead(des_pos):
+    global yaw_, pub, yaw_precision_, state_
+    desired_yaw = math.atan2(des_pos.y - position_.y, des_pos.x - position_.x)
+    err_yaw = desired_yaw - yaw_
+    err_pos = math.sqrt(pow(des_pos.y - position_.y, 2) +
+                        pow(des_pos.x - position_.x, 2))
+    err_yaw = normalize_angle(desired_yaw - yaw_)
+    rospy.loginfo(err_yaw)
+
+    if err_pos > dist_precision_:
+        twist_msg = Twist()
+        twist_msg.linear.x = 0.3
+        if twist_msg.linear.x > ub_d:
+            twist_msg.linear.x = ub_d
+
+        twist_msg.angular.z = kp_a*err_yaw
+        pub.publish(twist_msg)
+    else:
+        print ('Position error: [%s]' % err_pos)
+        change_state(2)
+
+    # state change conditions
+    if math.fabs(err_yaw) > yaw_precision_:
+        print ('Yaw error: [%s]' % err_yaw)
+        change_state(0)
+
+
+def done():
+    twist_msg = Twist()
+    twist_msg.linear.x = 0
+    twist_msg.angular.z = 0
+    pub.publish(twist_msg)
+
+
+def planning(goal):
+
+    global state_, desired_position_
+    global act_s
+
+    desired_position_.x = goal.target_pose.pose.position.x
+    desired_position_.y = goal.target_pose.pose.position.y
+
+    state_ = 0
+    rate = rospy.Rate(20)
+    success = True
+
+    feedback = robot_control.msg.RobotPlanningFeedback()
+    result = robot_control.msg.RobotPlanningResult()
+
+    while not rospy.is_shutdown():
+        if act_s.is_preempt_requested():
+            rospy.loginfo('Goal was preempted')
+            act_s.set_preempted()
+            success = False
+            break
+        elif state_ == 0:
+            feedback.stat = "Fixing the yaw"
+            feedback.position = pose_
+            act_s.publish_feedback(feedback)
+            fix_yaw(desired_position_)
+        elif state_ == 1:
+            feedback.stat = "Angle aligned"
+            feedback.position = pose_
+            act_s.publish_feedback(feedback)
+            go_straight_ahead(desired_position_)
+        elif state_ == 2:
+            feedback.stat = "Target reached!"
+            feedback.position = pose_
+            act_s.publish_feedback(feedback)
+            done()
+            break
+        else:
+            rospy.logerr('Unknown state!')
+
+        rate.sleep()
+    if success:
+        rospy.loginfo('Goal: Succeeded!')
+        act_s.set_succeeded(result)
+
+
+def main():
+    global pub, active_, act_s
+
+    rospy.init_node('go_to_point')
+
+    pub = rospy.Publisher('/robot/cmd_vel', Twist, queue_size=1)
+    sub_odom = rospy.Subscriber('/robot/odom', Odometry, clbk_odom)
+
+    act_s = actionlib.SimpleActionServer('/robot_reaching_goal', robot_control.msg.RobotPlanningAction, planning, auto_start=False)
+    act_s.start()
+
+    rate = rospy.Rate(20)
+
+    while not rospy.is_shutdown():
+        rate.sleep()
+
+
+if __name__ == '__main__':
+    main()
